7defa6a02e9292668c16aa1862e30f29bd1cf5ec
csh1102 <74477354+csh1102@users.noreply.github.com>
2024-07-10 15:38:52 +0100

    Deploy Flutter Web app to GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ee6bc19c00894f8eb3d52c0d552621ad
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "686f6b3bb2aa96e7aece0879b054eb86",
+"flutter_bootstrap.js": "e3b2765ab458ac7ce1f8028b3ecb92de",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
